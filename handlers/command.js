5bef13d7ac415b538ab93b4029f58d6cd0cc03ca
WoozyStudio <laurapinales412@gmail.com>
2023-04-04 16:08:34 -0600

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,24 +1,23 @@
-const fs = require("fs");
+const fs = require('fs');
+const ascii = require('ascii-table');
+let table = new ascii("Commands")
+table.setHeading('Commands', 'Load');
+require('colors');
 
 module.exports = (client) => {
-    try {
-        let comandos = 0;
-        fs.readdirSync("./commands/").forEach((carpeta) => {
-            const commands = fs.readdirSync(`./commands/${carpeta}`).filter((archivo) => archivo.endsWith(".js"))
-            for (let archivo of commands){
-                let comando = require(`../commands/${carpeta}/${archivo}`);
-                if(comando.name) {
-                    client.commands.set(comando.name, comando);
-                    comando++
-                } else {
-                    console.log(`The command [/${carpeta}/${archivo}]`, `error =>  Command not configured`)
-                    continue;
-                }
-                if(comando.aliases && Array.isArray(comando.aliases)) comando.aliases.forEach((alias) => client.aliases.set(alias, comando.name));
-            }
-        });
-        console.log(`${comandos} commands were successfully loaded!`);
-    } catch (e){
-        console.log(e)
-    }
+    fs.readdirSync('./commands/').forEach(dir => {
+        const commands = fs.readdirSync(`./commands/${dir}`).filter(file => file.endsWith('.js'));
+        for(let file of commands) {
+            let pull = require(`../commands/${dir}/${file}`);
+            if(pull.name) {
+                client.commands.set(pull.name, pull);
+                table.addRow(file, 'loaded')
+            } else {
+                table.addRow(file, 'Error - Missing a help.name or it is not a string...');
+                continue;
+            } if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name))
+        }
+    });
+
+    console.log(table.toString().red);
 }
