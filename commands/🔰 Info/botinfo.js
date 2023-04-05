8d1009f8f24748277411c312b48023652841fb85
WoozyStudio <laurapinales412@gmail.com>
2023-04-05 15:31:32 -0600

    Update botinfo.js

--- commands/🔰 Info/botinfo.js
+++ commands/🔰 Info/botinfo.js
@@ -7,7 +7,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         .setTitle("About Early Supporter")
         .setThumbnail("https://cdn.discordapp.com/avatars/1075181364669849651/1c6339e99acdab17ff1a719f48660cf8.png")
-        .setDescription("Hello, my name is Early Supporter and I am in charge of maintaining the server systems.")
+        .setDescription("Hi, I'm Early Supporter and I'm your personal assistant and helper on your Discord journey.")
         .setColor("BLURPLE")
         .addFields(
             { name: "Bot Name", value: "Early Supporter" },
