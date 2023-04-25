57e8f02d3eb32b0da7adb845da08fed3ff7cc84c
WoozyStudio <laurapinales412@gmail.com>
2023-04-24 21:41:28 -0600

    Update ReactionListener.java

--- java/ReactionListener.java
+++ java/ReactionListener.java
@@ -8,3 +8,113 @@ public class accept extends ListenerAdapter {
         channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
     }
 }
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
+public class accept extends ListenerAdapter {
+    public void onreactionAdded(MessageReactionAddEvent event) {
+
+        MessageReaction reaction = event.getReaction();
+        ReactionEmote emote = reaction.getReactionEmote();
+        MessageChannel channel = event.getChannel();
+        System.out.println("test");
+        channel.sendMessage("Commands:\n !notify - notifies users who have subscribed to mailing list").queue();
+    }
+}
