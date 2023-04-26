8f529cf201107a11ff70379b4a401676f2b97436
WoozyStudio <laurapinales412@gmail.com>
2023-04-26 08:36:35 -0600

    Update generate-keys.js

--- commands/⭐ Premium/generate-keys.js
+++ commands/⭐ Premium/generate-keys.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "generate-keys",
     alias: [],
     async execute(client, message, args){
-        if(!message.member.permissions.has("ADMINISTRATOR")) return message.reply("❌ `|` You do not have sufficient permissions to do this command.");
+        if(message.author.id !== '869583777884667964') return message.reply("❌ `|`You do not have sufficient permissions to use this command. Try again later!")
         let keys = ["KV16kpu905", "XIu53RhO91", "qKmp35Y98A", "973HYpq0Nm", "63tHB9AxMq", "poAxYe872x", "A9172TwpLsZ", "U40KmmnLeq", "8e6PlaEq81", "yHam32Ne9z"]
         let generate_random_keys = keys[Math.floor(Math.random() * keys.length)]
 
