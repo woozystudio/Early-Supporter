f1de5a81b9b4ea8a945d0d762b73785d0b535764
WoozyStudio <laurapinales412@gmail.com>
2023-05-01 08:18:36 -0600

    Update ping.js

--- slashCommands/🔰 Information/ping.js
+++ slashCommands/🔰 Information/ping.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Returns with the bot ping'),
+    .setDescription('Returns with the bot ping.'),
     async run(client, interaction) {
         interaction.reply({ content: `Pong! The API ping is **${client.ws.ping}ms**` })
     }
