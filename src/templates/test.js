5fdf9b3a5bfb8dc99179006bbfc8b9753d293366
WoozyStudio <laurapinales412@gmail.com>
2023-05-03 16:30:26 -0600

    Delete src/template directory

--- src/templates/test.js
+++
@@ -1,7 +0,0 @@
-const ascii = require('ascii-table');
-let table = new ascii("Commands")
-table.setHeading('Commands', 'Load');
-require('colors');
-table.addRow('loaded')
-table.addRow('Error - Missing a help.name or it is not a string...');
-console.log(table.toString().red);
