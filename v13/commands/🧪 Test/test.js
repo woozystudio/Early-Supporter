aadf9c1252e8df5ecb6a02a41fff34fd9fb8c760
WoozyStudio <laurapinales412@gmail.com>
2023-05-12 18:58:06 -0600

    Remove v13 code and start a new code in v14

--- v13/commands/🧪 Test/test.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = {
-    name: 'test',
-    aliases: [],
-    async execute(client, message, args) {
-        message.channel.send(`Everything working fine for the moment <@${message.author.id}>!`)
-    }
-}
