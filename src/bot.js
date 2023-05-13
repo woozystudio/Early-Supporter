8eb30ecf50a6dad3590ed8fd713389a411eb9725
WoozyStudio <laurapinales412@gmail.com>
2023-05-12 20:30:30 -0600

    Create bot.js

---
+++ src/bot.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const config = require('../config/config.json');
+const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+require('colors');
+
+const client = new Discord.Client({
+    intents: [
+        GatewayIntentBits.AutoModerationConfiguration,
+        GatewayIntentBits.AutoModerationExecution,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildScheduledEvents,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.GuildScheduledEvent,
+        Partials.Message,
+        Partials.Reaction,
+        Partials.ThreadMember,
+        Partials.User,
+    ],
+});
+
+client.commands = new Collection();
+client.commandArray = [];
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+    const functionFiles = fs.readdirSync(`./src/functions/${folder}`).filter((file) => file.endsWith('.js'));
+    for (const file of functionFiles) {
+        require(`./functions/${folder}/${file}`)(client);
+    }
+}
+
+client.handleEvents();
+client.handleCommands();
+client.login(config.token);
